13b5fe804f967d6f35eb5436d4c587f59041c077
satish <sathish.sunchu@valuelabs.com>
2014-05-02 18:17:29 +0530

    changes in index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    
+    #@uname = params['uname']
+    #@uname = "document.getElementById('login').innerHTML = 'Hello'"
+  end
+
+  def success
+    @uname = params['uname']
   end
 
 end
